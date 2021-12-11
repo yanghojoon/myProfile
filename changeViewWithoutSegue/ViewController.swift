f3227461ce4c299506b15f764100ce9de6e98ca5
yanghojoon <yanghojoon8487@gmail.com>
2021-12-11 12:43:49 +0900

    feat: nameLabel의 오토레이아웃 설정 타입>: <제목>

--- changeViewWithoutSegue/ViewController.swift
+++ changeViewWithoutSegue/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var descriptionButton: UIButton?
+    @IBOutlet weak var nameLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         descriptionButton?.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel?.translatesAutoresizingMaskIntoConstraints = false
         
         let safeArea = view.safeAreaLayoutGuide
         
@@ -23,6 +25,8 @@ class ViewController: UIViewController {
         safeBottomAnchor?.priority = .defaultHigh
         
         descriptionButton?.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -30).isActive = true
+        
+        nameLabel?.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
     }
 
 
